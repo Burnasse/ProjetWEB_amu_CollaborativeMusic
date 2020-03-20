24d085abbcc4b88cd9b3f351b78987235aa83241
Burnasse <tommy.lecourt@etu.univ-amu.fr>
2020-03-20 20:50:16 +0100

    add background and update README.md

--- src/ChatComponent/Join/Join.js
+++ src/ChatComponent/Join/Join.js
@@ -9,7 +9,7 @@ const Join = () => {
     return(
         <div className="joinOut">
             <div className="joinIn">
-                <h1 className="head">Join</h1>
+                <h1 className="head">Join a music room</h1>
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(event => setName(event.target.value))}/></div>
                 <div><input placeholder="Room" className="joinInput" type="text" onChange={(event => setRoom(event.target.value))}/></div>
                 <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/musicroom?name=${name}&room=${room}`}>
