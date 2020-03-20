cf7e6ec6efb222c5566ac96ca7f6eb20b3c2dd00
Burnasse <tommy.lecourt@etu.univ-amu.fr>
2020-03-20 15:23:52 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Route} from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Join from './ChatComponent/Join/Join';
 import MusicRoom from "./MusicRoom";
